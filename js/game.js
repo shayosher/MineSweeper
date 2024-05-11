29795921038b0a0d4a419f698661fa2fc216c600
shayosher <129197476+shayosher@users.noreply.github.com>
2024-05-11 21:46:26 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -18,22 +18,23 @@ var gLevel = {
     forVictory: 14
 }
 var gTimerInterval
-var gHintMode
+var gManuallyMinesMode = false
 
 function onInit() {
 
+
     restartTimer()
-    gHintMode = false
     gGame = {
         isOn: true,
         shownCount: 0,
         markedCount: 0,
         secsPassed: 0,
-        lives: 3
+        lives: gLevel.mines > 2 ? 3 : 2,
+        hintMode: false,
+        hintsCount: 3,
+        safeClicks: 3
     }
     gBoard = buildBoard()
-    // setMinesLocations()
-    // setMinesNegsCount()
     renderBoard()
 
     handelSmiley(NORMAL)
@@ -100,7 +101,6 @@ function renderBoard() {
     for (var i = 0; i < gBoard.length; i++) {
         strHTML += '<tr>'
         for (var j = 0; j < gBoard[0].length; j++) {
-            const cell = ""
             const className = `cell cell-${i}-${j}`
             strHTML += `<td class="${className}" onclick="onCellClicked(this,${i},${j})" oncontextmenu="onCellMarked(event, ${i},${j})"></td>`
         }
@@ -122,7 +122,7 @@ function onCellClicked(elCell, i, j) {
     if (cell.isMarked) return
     if (cell.isShown) return
 
-    if (gHintMode) {
+    if (gGame.hintMode) {
         handelHint(elCell, i, j)
         return
     }
@@ -197,22 +197,6 @@ function onCellMarked(event, i, j) {
     }
 }
 
-function checkGameOver(elCell) {
-    renderLives()
-    if (!gGame.lives) { //(gGame.lives === 0)
-        elCell.style.backgroundColor = 'red'
-        for (var i = 0; i < gBoard.length; i++) {
-            for (var j = 0; j < gBoard[i].length; j++) {
-                if (gBoard[i][j].isMine === true) {
-                    renderCell({ i, j }, MINE)
-                    elCell.classList.add("cell-isClicked")
-                }
-            }
-        }
-        gameOver()
-    }
-}
-
 function renderLives() {
     var livesStr = ''
     for (let i = 0; i < gGame.lives; i++) {
@@ -254,77 +238,6 @@ function restartTimer() {
     clearInterval(gTimerInterval)
     document.querySelector('.minutes').innerText = '00'
     document.querySelector('.seconds').innerText = '00'
-}
-
-function checkVictory() {
-    if (gGame.shownCount === gLevel.forVictory && gGame.markedCount === gLevel.mines) {
-        clearInterval(gTimerInterval)
-
-        const minutes = +document.querySelector('.minutes').innerText
-        const seconds = +document.querySelector('.seconds').innerText
-        const time = { minutes, seconds }
-
-        if (gLevel.size === 4) {
-            const bestMin = localStorage.getItem("score16Min")
-            const bestSec = localStorage.getItem("score16Sec")
-            if (!bestMin || time.minutes < bestMin) {
-                localStorage.setItem("score16Min", time.minutes)
-                localStorage.setItem("score16Sec", time.seconds)
-            }
-            if (time.minutes === bestMin) {
-                if (time.seconds < bestSec) {
-                    localStorage.setItem("score16Min", time.minutes)
-                    localStorage.setItem("score16Sec", time.seconds)
-                }
-            }
-        }
-
-        if (gLevel.size === 8) {
-            const bestMin = localStorage.getItem("score64Min")
-            const bestSec = localStorage.getItem("score64Sec")
-            if (!bestMin || time.minutes < bestMin) {
-                localStorage.setItem("score64Min", time.minutes)
-                localStorage.setItem("score64Sec", time.seconds)
-            }
-            if (time.minutes === bestMin) {
-                if (time.seconds < bestSec) {
-                    localStorage.setItem("score64Min", time.minutes)
-                    localStorage.setItem("score64Sec", time.seconds)
-                }
-            }
-        }
-
-        if (gLevel.size === 12) {
-            const bestMin = localStorage.getItem("score144Min")
-            const bestSec = localStorage.getItem("score144Sec")
-            if (!bestMin || time.minutes < bestMin) {
-                localStorage.setItem("score144Min", time.minutes)
-                localStorage.setItem("score144Sec", time.seconds)
-            }
-            if (time.minutes === bestMin) {
-                if (time.seconds < bestSec) {
-                    localStorage.setItem("score144Min", time.minutes)
-                    localStorage.setItem("score144Sec", time.seconds)
-                }
-            }
-        }
-
-        handelSmiley(WIN)
-        addBestScore()
-        gGame.isOn = false
-    }
-}
-function addBestScore() {
-
-    document.querySelector(".score16Best").innerText = (localStorage.getItem("score16Min") + ':' + localStorage.getItem("score16Sec"))
-    document.querySelector(".score64Best").innerText = (localStorage.getItem("score64Min") + ':' + localStorage.getItem("score64Sec"))
-    document.querySelector(".score144Best").innerText = (localStorage.getItem("score144Min") + ':' + localStorage.getItem("score144Sec"))
-}
-
-function gameOver() {
-    restartTimer()
-    handelSmiley(DEAD)
-    gGame.isOn = false
 }
 
 function handelHint(elCell, cellI, cellJ) {
@@ -382,15 +295,17 @@ function handelHint(elCell, cellI, cellJ) {
         }
     }, 1000)
 
-    gHintMode = false
+    gGame.hintMode = false
 }
 
-function onHint(elHint) {
+function onHintBtn(elHint) {
 
     if (!gGame.shownCount) return
+    if (gGame.hintsCount === 0) return
 
+    gGame.hintsCount--
     elHint.innerHTML = '<img src="img/bulbOn.jpg">'
-    gHintMode = true
+    gGame.hintMode = true
 }
 function resHint() {
     const elHints = document.querySelectorAll('.ints button')
@@ -400,7 +315,7 @@ function resHint() {
 }
 
 function handelSmiley(state) {
-    document.querySelector('.smiley').innerText = state
+    document.querySelector('.smiley-btn').innerText = state
 }
 
 function handelShownCount() {
@@ -428,11 +343,125 @@ function onLevelBtn(value) {
             gLevel.forVictory = 112
             break
     }
+    gGame.isOn = false
+    gManuallyMinesMode = false
+    clearInterval(gTimerInterval)
     onInit()
 }
 
+
+function checkGameOver(elCell) {
+    renderLives()
+    if (!gGame.lives) { //(gGame.lives === 0)
+        revealAllMines(elCell)
+        gameOver()
+    }
+}
+
+function revealAllMines(elCell) {
+    elCell.style.backgroundColor = 'red'
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[i].length; j++) {
+            if (gBoard[i][j].isMine === true) {
+                renderCell({ i, j }, MINE)
+                elCell.classList.add("cell-isClicked")
+            }
+        }
+    }
+}
+
+function gameOver() {
+    clearInterval(gTimerInterval)
+    handelSmiley(DEAD)
+    gManuallyMinesMode = false
+    gGame.isOn = false
+}
+
 function restart() {
+    gGame.isOn = false
+    gManuallyMinesMode = false
+    if (gLevel.size === 4) {
+        gLevel.mines = 2
+        gLevel.forVictory = 14
+    }
+
+    if (gLevel.size === 8) {
+        gLevel.mines = 14
+        gLevel.forVictory = 50
+    }
+
+    if (gLevel.size === 12) {
+        gLevel.mines = 32
+        gLevel.forVictory = 112
+    }
+
     onInit()
+}
+
+function checkVictory() {
+    if (gGame.shownCount === gLevel.forVictory && gGame.markedCount === gLevel.mines) {
+        clearInterval(gTimerInterval)
+
+        const minutes = +document.querySelector('.minutes').innerText
+        const seconds = +document.querySelector('.seconds').innerText
+        const time = { minutes, seconds }
+
+        if (gLevel.size === 4) {
+            const bestMin = localStorage.getItem("score16Min")
+            const bestSec = localStorage.getItem("score16Sec")
+            if (!bestMin || time.minutes < bestMin) {
+                localStorage.setItem("score16Min", time.minutes)
+                localStorage.setItem("score16Sec", time.seconds)
+            }
+            if (time.minutes === bestMin) {
+                if (time.seconds < bestSec) {
+                    localStorage.setItem("score16Min", time.minutes)
+                    localStorage.setItem("score16Sec", time.seconds)
+                }
+            }
+        }
+
+        if (gLevel.size === 8) {
+            const bestMin = localStorage.getItem("score64Min")
+            const bestSec = localStorage.getItem("score64Sec")
+            if (!bestMin || time.minutes < bestMin) {
+                localStorage.setItem("score64Min", time.minutes)
+                localStorage.setItem("score64Sec", time.seconds)
+            }
+            if (time.minutes === bestMin) {
+                if (time.seconds < bestSec) {
+                    localStorage.setItem("score64Min", time.minutes)
+                    localStorage.setItem("score64Sec", time.seconds)
+                }
+            }
+        }
+
+        if (gLevel.size === 12) {
+            const bestMin = localStorage.getItem("score144Min")
+            const bestSec = localStorage.getItem("score144Sec")
+            if (!bestMin || time.minutes < bestMin) {
+                localStorage.setItem("score144Min", time.minutes)
+                localStorage.setItem("score144Sec", time.seconds)
+            }
+            if (time.minutes === bestMin) {
+                if (time.seconds < bestSec) {
+                    localStorage.setItem("score144Min", time.minutes)
+                    localStorage.setItem("score144Sec", time.seconds)
+                }
+            }
+        }
+
+        handelSmiley(WIN)
+        addBestScore()
+        gManuallyMinesMode = false
+        gGame.isOn = false
+    }
+}
+function addBestScore() {
+
+    document.querySelector(".score16Best").innerText = (localStorage.getItem("score16Min") + ':' + localStorage.getItem("score16Sec"))
+    document.querySelector(".score64Best").innerText = (localStorage.getItem("score64Min") + ':' + localStorage.getItem("score64Sec"))
+    document.querySelector(".score144Best").innerText = (localStorage.getItem("score144Min") + ':' + localStorage.getItem("score144Sec"))
 }
 
 function renderCell(location, value) {
